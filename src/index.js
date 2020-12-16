540eb456f07f52c7b4c42b14f2a7002956c99416
syudachin <70334865+syudachin@users.noreply.github.com>
2020-12-16 23:29:41 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,78 @@
-import "./styles.css";
+//const let等の変数宣言
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き";
+// console.log("val2");
+
+// let val2 = "letを再宣言";
+
+// const val3 = "const変数";
+
+// console.log(val3);
+// // val3 = "uwagaki";
+
+// const val3 = "const変数を再宣言";
+
+// const で定義したobjectはプロパティの変更が可能
+// const val4 = {
+//   name: "jaken",
+//   age: "28"
+// };
+// val4.name = "jak";
+// val4.address = "hiroshima";
+// console.log(val4);
+
+// // const で定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+// const name = "じゅん";
+// const age = 33;
+
+// const message3 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message3);
+
+//アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
+
+//分割代入
+// const myProfile = {
+//   name: "しゅだちん",
+//   age: 34
+// };
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// // console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// const myProfile = ["しゅだちん", 22];
+// const message3 = `名前は${myProfile[0]}でし。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
